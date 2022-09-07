c14a14652b4f381c31421d6475e1407359ab9b4b
aster <aster.niene@gmail.com>
2022-09-07 14:12:14 +0200

    add pytest.ini

--- testStep3/test_abs_project.py
+++ testStep3/test_abs_project.py
@@ -8,6 +8,16 @@ class TestAbs(unittest.TestCase):
     def test_abs2(self):
         self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
+    def test_abs3(self):
+        assert abs(-42) == 42, "Should be absolute value of a number"
+
+    def test_abs4(self):
+        assert abs(-42) == -42, "Should be absolute value of a number"
+    def test_abs5(self):
+        a = 4
+        b = 5
+        assert a == b, "Значения разные"
+
 
 if __name__ == "__main__":
     unittest.main()
